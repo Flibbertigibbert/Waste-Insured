1bd4dea8fe2212f0429665b6d4ab8be7a5c785e0
amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-08-04 02:13:41 -0700

    udpate the verification if a waste is paid

--- frontend/src/components/WasteFilter.jsx
+++ frontend/src/components/WasteFilter.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const WasteFilter = ({ onSearch }) => {
   return (
-    <div>
-        <input type="text" onChange={(e) => onSearch(e.target.value)} placeholder="Enter the name of the records" name="searchBar" id="searchBar" />
+    <div className='flex justify-center '>
+        <input type="text" className='border-[#EFAE07] border-2 rounded-3xl py-2 px-5' onChange={(e) => onSearch(e.target.value)} placeholder="Search Records" name="searchBar" id="searchBar" />
     </div>
   )
 }
