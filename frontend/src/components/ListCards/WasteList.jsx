b006e1b039327f438847c936f57835bc56ff30d8
amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-08-04 16:14:45 -0700

    fix error message

--- frontend/src/components/ListCards/WasteList.jsx
+++ frontend/src/components/ListCards/WasteList.jsx
@@ -58,9 +58,9 @@ const WasteList = () => {
   return (
     <div>
       <WasteFilter onSearch={handleSearch} />
-      {error && <ErrorAlert message={error} clear={clearmessage} />}
+      {/* {error && <ErrorAlert message={error} clear={clearmessage} />}
       {success && <OnSuccessAlert message={success} />}
-      {loading && <LoadingAlert message={loading} />}
+      {loading && <LoadingAlert message={loading} />} */}
         <div className=' mx-auto max-w-4xl py-5 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
           <div className='grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8'>
             {getWasteLength()}
