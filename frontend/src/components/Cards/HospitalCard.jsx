6983d0a706b42c18599669e792fba609b394be6b
Amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-10-05 23:37:29 -0700

    feat: hopital components

---
+++ frontend/src/components/Cards/HospitalCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HospitalCard = () => {
+  return (
+    <div>
+      Hospital Card
+    </div>
+  )
+}
+
+export default HospitalCard
---
+++ frontend/src/components/ListCards/HospitalList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HospitalList = () => {
+  return (
+    <div>
+      Hospital List
+    </div>
+  )
+}
+
+export default HospitalList
